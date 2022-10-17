9efad7dc4d630c9b6e54ae315bdcbcd3137bf7ee
SiyaDavra <115966635+SiyaDavra@users.noreply.github.com>
2022-10-17 10:02:58 +0530

    Add files via upload

---
+++ jp1c.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+import java.util.Scanner;
+public class jp1c {
+public static void main(String[] args) {
+        int num;
+        Scanner in = new Scanner(System.in);
+       
+        System.out.println("Enter integer: ");
+        num= in.nextInt();
+        System.out.println("Input integer: "+num);
+    }
+    
+}
